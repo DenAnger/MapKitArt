964673c59b45eede1b5e78861aa951a993f91539
Denis Abramov <DenAnger@outlook.com>
2024-05-26 23:08:44 +0700

    Refactoring

--- MapKitArt/Controllers/ViewController.swift
+++ MapKitArt/Controllers/ViewController.swift
@@ -10,81 +10,96 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet private var mapView: MKMapView!
-    
-    private var artworks: [Artwork] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set initial location in Honolulu
-        let initialLocation = CLLocation(latitude: 21.282778,
-                                         longitude: -157.829444)
-        mapView.centerToLocation(initialLocation)
-        
-        let oahuCenter = CLLocation(latitude: 21.4765, longitude: -157.9647)
-        let region = MKCoordinateRegion(center: oahuCenter.coordinate,
-                                        latitudinalMeters: 50000,
-                                        longitudinalMeters: 60000)
-        mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region),
-                                  animated: true)
-        
-        let zoomRage = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200_000)
-        mapView.setCameraZoomRange(zoomRage, animated: true)
-        
-        mapView.delegate = self
-        
-        // Annotations with Images
-        mapView.register(ArtworkView.self,
-                         forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
-        
-        loadInitialData()
-        mapView.addAnnotations(artworks)
-    }
-    
-    private func loadInitialData() {
-        guard
-            let fileName = Bundle.main.url(forResource: "PublicArt",
-                                           withExtension: "geojson"),
-        let artworkData = try? Data(contentsOf: fileName)
-        else {
-            return
-        }
-        
-        do {
-            let features = try MKGeoJSONDecoder()
-            .decode(artworkData)
-            .compactMap { $0 as? MKGeoJSONFeature }
-            let validWorks = features.compactMap(Artwork.init)
-            artworks.append(contentsOf: validWorks)
-        } catch {
-            print("Unexpected error: \(error).")
-        }
-    }
+	
+	@IBOutlet private var mapView: MKMapView!
+	
+	private var artworks: [Artwork] = []
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Set initial location in Honolulu
+		let initialLocation = CLLocation(
+			latitude: 21.282778,
+			longitude: -157.829444
+		)
+		mapView.centerToLocation(initialLocation)
+		
+		let oahuCenter = CLLocation(latitude: 21.4765, longitude: -157.9647)
+		let region = MKCoordinateRegion(
+			center: oahuCenter.coordinate,
+			latitudinalMeters: 50000,
+			longitudinalMeters: 60000
+		)
+		mapView.setCameraBoundary(
+			MKMapView.CameraBoundary(coordinateRegion: region),
+			animated: true
+		)
+		
+		let zoomRage = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200_000)
+		mapView.setCameraZoomRange(zoomRage, animated: true)
+		
+		mapView.delegate = self
+		
+		// Annotations with Images
+		mapView.register(
+			ArtworkView.self,
+			forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier
+		)
+		
+		loadInitialData()
+		mapView.addAnnotations(artworks)
+	}
+	
+	private func loadInitialData() {
+		
+		guard let fileName = Bundle.main.url(
+			forResource: "PublicArt",
+			withExtension: "geojson"
+		),
+			  let artworkData = try? Data(contentsOf: fileName) else {
+			return
+		}
+		
+		do {
+			let features = try MKGeoJSONDecoder()
+				.decode(artworkData)
+				.compactMap { $0 as? MKGeoJSONFeature }
+			let validWorks = features.compactMap(Artwork.init)
+			artworks.append(contentsOf: validWorks)
+		} catch {
+			print("Unexpected error: \(error).")
+		}
+	}
 }
 
 private extension MKMapView {
-    func centerToLocation(_ location: CLLocation,
-    regionRadius: CLLocationDistance = 1000) {
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
-                                                  latitudinalMeters: regionRadius,
-                                                  longitudinalMeters: regionRadius)
-        setRegion(coordinateRegion, animated: true)
-    }
+	func centerToLocation(
+		_ location: CLLocation,
+		regionRadius: CLLocationDistance = 1000
+	) {
+		let coordinateRegion = MKCoordinateRegion(
+			center: location.coordinate,
+			latitudinalMeters: regionRadius,
+			longitudinalMeters: regionRadius
+		)
+		setRegion(coordinateRegion, animated: true)
+	}
 }
 
 extension ViewController: MKMapViewDelegate {
-
-    func mapView(_ mapView: MKMapView,
-                 annotationView view: MKAnnotationView,
-                 calloutAccessoryControlTapped control: UIControl) {
-        
-        guard let artwork = view.annotation as? Artwork else {
-            return
-        }
-        
-        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
-        artwork.mapItem?.openInMaps(launchOptions: launchOptions)
-    }
+	
+	func mapView(
+		_ mapView: MKMapView,
+		annotationView view: MKAnnotationView,
+		calloutAccessoryControlTapped control: UIControl
+	) {
+		
+		guard let artwork = view.annotation as? Artwork else {
+			return
+		}
+		
+		let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+		artwork.mapItem?.openInMaps(launchOptions: launchOptions)
+	}
 }
--- MapKitArt/Models/ArtworkViews.swift
+++ MapKitArt/Models/ArtworkViews.swift
@@ -11,45 +11,54 @@ import MapKit
 
 // Markers with Color-Coding and Text
 class ArtworkMarkerView: MKMarkerAnnotationView {
-    override var annotation: MKAnnotation? {
-        willSet {
-            guard let artwork = newValue as? Artwork else {
-                return
-            }
-            canShowCallout = true
-            calloutOffset = CGPoint(x: -5, y: 5)
-            rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-            
-            markerTintColor = artwork.markerTintColor
-
-            glyphImage = artwork.image
-        }
-    }
+	override var annotation: MKAnnotation? {
+		willSet {
+			
+			guard let artwork = newValue as? Artwork else {
+				return
+			}
+			
+			canShowCallout = true
+			calloutOffset = CGPoint(x: -5, y: 5)
+			rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+			
+			markerTintColor = artwork.markerTintColor
+			
+			glyphImage = artwork.image
+		}
+	}
 }
 
 // Annotations with Images
 class ArtworkView: MKAnnotationView {
-    override var annotation: MKAnnotation? {
-        willSet {
-            guard let artwork = newValue as? Artwork else {
-                return
-            }
-            canShowCallout = true
-            calloutOffset = CGPoint(x: -5, y: 5)
-
-            let mapsButton = UIButton(frame: CGRect(origin: CGPoint.zero,
-                                                    size: CGSize(width: 48,
-                                                                 height: 48)))
-            mapsButton.setBackgroundImage(#imageLiteral(resourceName: "Map"), for: .normal)
-            rightCalloutAccessoryView = mapsButton
-            
-            image = artwork.image
-            
-            let detailLabel = UILabel()
-            detailLabel.numberOfLines = 0
-            detailLabel.font = detailLabel.font.withSize(12)
-            detailLabel.text = artwork.subtitle
-            detailCalloutAccessoryView = detailLabel
-        }
-    }
+	override var annotation: MKAnnotation? {
+		willSet {
+			
+			guard let artwork = newValue as? Artwork else {
+				return
+			}
+			
+			canShowCallout = true
+			calloutOffset = CGPoint(x: -5, y: 5)
+			
+			let mapsButton = UIButton(frame: CGRect(
+				origin: CGPoint.zero,
+				size: CGSize(width: 48,
+							 height: 48))
+			)
+			mapsButton.setBackgroundImage(
+				#imageLiteral(resourceName: "Map"),
+				for: .normal
+			)
+			rightCalloutAccessoryView = mapsButton
+			
+			image = artwork.image
+			
+			let detailLabel = UILabel()
+			detailLabel.numberOfLines = 0
+			detailLabel.font = detailLabel.font.withSize(12)
+			detailLabel.text = artwork.subtitle
+			detailCalloutAccessoryView = detailLabel
+		}
+	}
 }
